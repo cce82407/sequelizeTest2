7e6645fed90ca251a3cb132cd34f19023bbfc73d
Cynthia E <cynthia8499@hotmail.com>
2020-06-29 18:58:25 -0400

    sequelize 2 fail

--- pumpkin.js
+++ pumpkin.js
@@ -1,7 +1,33 @@
 const db = require('./dbConfig');
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes, Model } = require('sequelize');
 
-//YOUR CODE GOES HERE
 
-//--------------------
+const Pumpkin = db.define({
+
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  size: {
+    type: DataTypes.STRING
+  },
+  evil: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+  carved: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  candle: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+})
+
+
+
+
+// the defined model is the class itself
+console.log(User === sequelize.models.User); // true
 module.exports = Pumpkin;
